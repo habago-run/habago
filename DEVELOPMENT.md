d5ac5536f9cf5414bf951e29c782cb0d48494100
yu.wang <wangyu@wycode.cn>
2025-03-15 21:43:31 +0800

    development.md

---
+++ DEVELOPMENT.md
@@ -0,0 +1,18 @@
+# 开发环境
+
+使用最新版的
+
+- node.js
+- pnpm
+
+## 安装依赖
+
+```bash
+pnpm install
+```
+
+## 启动开发环境
+
+```bash
+pnpm admin:dev
+```
--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -13,10 +13,13 @@ export default function Home() {
         <h1 className="animate-gradient mx-auto text-center text-6xl font-bold text-transparent">
           哈巴狗
         </h1>
+        <h2 className="mt-4 text-center text-2xl font-medium text-slate-800 transition-colors dark:text-slate-100">
+          面向AI的下一代全栈框架，0停机更新功能。
+        </h2>
         <p className="mt-8 text-center text-slate-800 transition-colors dark:text-slate-100">
           尚未配置首页插件，去
           <Link
-            className="mx-1 font-bold text-blue-600 underline"
+            className="mx-1 font-bold text-blue-600 dark:text-blue-400 underline transition-colors"
             href="/admin/dashboard"
           >
             后台管理
